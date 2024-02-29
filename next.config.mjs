6eadcbd6212c609b7c9e5f635890057edc8b8f18
emre durgunlu <emredurgunlu@gmail.com>
2024-03-01 01:41:10 +0300

    nextjs config for static deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:"https",
-                hostname:"images.pexels.com",
-            }
-        ]
-    }
+    // output: "export", is for static deploying
+  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
