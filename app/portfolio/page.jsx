f87134adcf3ed23f56a084af77135bbd67a47a4e
emre durgunlu <emredurgunlu@gmail.com>
2024-02-29 16:06:33 +0300

    portfolio page my works title

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,7 +1,11 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 const PortfolioPage = () => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({ target: ref });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
   return (
     <motion.div
       className="h-full"
@@ -9,7 +13,12 @@ const PortfolioPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      PortfolioPage
+      <div className="h-[600vh]" ref={ref}>
+        {/*6rem navbar yüksekliği*/}
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+          My Works
+        </div>
+      </div>
     </motion.div>
   );
 };
