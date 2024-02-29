8339cdf3192bc2bab9f42c92a8b9e5f1b0ffb796
emre durgunlu <emredurgunlu@gmail.com>
2024-03-01 00:39:46 +0300

    contact page text animation

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 
 const ContactPage = () => {
+  const text = "Say Hello";
   return (
     <motion.div
       className="h-full"
@@ -9,7 +10,30 @@ const ContactPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      ContactPage
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            😊
+          </div>
+        </div>
+        {/* FORM CONTAINER */}
+        <form className="h-1/2 lg:h-full lg:w-1/2"></form>
+      </div>
     </motion.div>
   );
 };
