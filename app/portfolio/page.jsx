e08f52685d53ef544310acf2aec5b50860ba4053
emre durgunlu <emredurgunlu@gmail.com>
2024-03-01 00:10:06 +0300

    portfolio page bottom part animation

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -83,7 +83,9 @@ const PortfolioPage = () => {
       <div className="h-screen w-screen flex flex-col gap-16 items-center justify-center text-center">
         <h1 className="text-8xl">Do you have a project?</h1>
         <div className="relative">
-          <svg
+          <motion.svg
+            animate={{ rotate: 360 }}
+            transition={{ duration: 8, ease: "linear", repeat: Infinity }}
             viewBox="0 0 300 300"
             className="w-64 h-64 md:w-[500px] md:h-[500px] "
           >
@@ -99,10 +101,10 @@ const PortfolioPage = () => {
                 Front-end Developer and UI Designer
               </textPath>
             </text>
-          </svg>
+          </motion.svg>
           <Link
             href="/contact"
-            className="w-16 h-16 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
           >
             Hire Me
           </Link>
