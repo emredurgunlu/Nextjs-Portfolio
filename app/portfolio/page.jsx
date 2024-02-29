5756a5de94533342eace6cf269d719c2cd079c38
emre durgunlu <emredurgunlu@gmail.com>
2024-03-01 00:06:31 +0300

    portfolio page bottom part style

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -58,8 +58,8 @@ const PortfolioPage = () => {
         </div>
         <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
           {/* style={{x:x}} ile style={{x}} aynı. overflow-hidden yatay scroll barı engelledi*/}
-          <motion.div style={{x}} className="flex">
-            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300"/>
+          <motion.div style={{ x }} className="flex">
+            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
             {items.map((item) => (
               <div
                 className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
@@ -78,6 +78,34 @@ const PortfolioPage = () => {
               </div>
             ))}
           </motion.div>
+        </div>
+      </div>{" "}
+      <div className="h-screen w-screen flex flex-col gap-16 items-center justify-center text-center">
+        <h1 className="text-8xl">Do you have a project?</h1>
+        <div className="relative">
+          <svg
+            viewBox="0 0 300 300"
+            className="w-64 h-64 md:w-[500px] md:h-[500px] "
+          >
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+              />
+            </defs>
+            <text fill="#000">
+              {/*xlinkHref="#circlePath" ile textPath'e yukardaki circle id'yi verdik ki textimiz bu circle pathi takip etsin */}
+              <textPath xlinkHref="#circlePath" className="text-xl">
+                Front-end Developer and UI Designer
+              </textPath>
+            </text>
+          </svg>
+          <Link
+            href="/contact"
+            className="w-16 h-16 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+          >
+            Hire Me
+          </Link>
         </div>
       </div>
     </motion.div>
